be5f186bda16b6e594f9eccd635f16ce74c7ce2c
munteanudenis87 <munteanudenis87@gmail.com>
2025-12-17 22:28:19 +0100

    aggiunto bottone spegni funzionante

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const accendiButtonEl = document.getElementById("accendi-btn");
 
 const yellowLamp = document.getElementById("yellow-lamp");
 const whiteLamp = document.getElementById("white-lamp");
+const spegniButtonEl = document.getElementById("spegni-btn");
 
 accendiButtonEl.addEventListener("click", function(){
     yellowLamp.classList.add("active");
     whiteLamp.classList.add("active");
+    spegniButtonEl.classList.add("active");
+    accendiButtonEl.classList.add("active");
+});
+
+spegniButtonEl.addEventListener("click", function(){
+    accendiButtonEl.classList.remove("active");
+    whiteLamp.classList.remove("active");
+    yellowLamp.classList.remove("active");
+    spegniButtonEl.classList.remove("active");
+
 });
